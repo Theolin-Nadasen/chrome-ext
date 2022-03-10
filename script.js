04c77629426aad4e4caab097fc3d6261abbe5c75
P5YCH0x <psychox.jeff@gmail.com>
2022-03-11 00:05:27 +0200

    added get tab and delete buttons

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-button"); 
 const leadList = document.getElementById("leads");
+const deleteBtn = document.getElementById("delete");
+const tabBtn = document.getElementById("add-tab");
 
 if(localStorage.getItem("myLeads")){
     myLeads = JSON.parse(localStorage.getItem("myLeads"));
@@ -13,16 +15,30 @@ inputBtn.addEventListener("click", () => {
     if(inputEl.value){
         myLeads.push(inputEl.value);
         inputEl.value = "";
-        leadList.innerHTML = "";
         addLead();
         localStorage.setItem("myLeads", JSON.stringify(myLeads));
     }
-})
+});
+
+deleteBtn.addEventListener("click", () => {
+    localStorage.clear();
+    myLeads = [];
+    leadList.innerHTML = "";
+});
+
+tabBtn.addEventListener("click", () => {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        addLead();
+    });
+});
 
 function addLead(){
+    leadList.innerHTML = "";
     for(let i = 0; i < myLeads.length; i++){
         let lead = document.createElement("li");
-        lead.innerHTML = `<a href="#">${myLeads[i]}</a>`;
+        lead.innerHTML = `<a href="${myLeads[i]}>${myLeads[i]}</a>`;
         leadList.appendChild(lead);
     }
 }
